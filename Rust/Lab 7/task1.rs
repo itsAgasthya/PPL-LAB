a4b439dd3549f171b4db498bb34fab888046c66d
Agasthya <78071231+itsAgasthya@users.noreply.github.com>
2025-02-21 13:28:54 +0530

    Add files via upload

---
+++ Rust/Lab 7/task1.rs
@@ -0,0 +1,13 @@
+fn main() {
+    // Declare variables of different datatypes
+    let integer: u32 = 42;
+    let floating_point: f64 = 3.14;
+    let boolean: bool = true;
+    let character: char = 'R';
+
+    // Print
+    println!("Integer: {}", integer);
+    println!("Floating-point: {}", floating_point);
+    println!("Boolean: {}", boolean);
+    println!("Character: {}", character);
+}
---
+++ Rust/Lab 7/task6.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // Using a while loop to print odd numbers from 1 to 20
+    let mut i = 1;
+    while i <= 20 {
+        println!("{}", i);
+        i += 2; // Increment by 2 to get the next odd number
+    }
+}
